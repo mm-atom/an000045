566d21af425011a5b715c29d82014126c852347d
taoqf <tao_qiufeng@126.com>
2020-12-21 15:29:23 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-export default function fun() {
+import { IncomingMessage } from 'http';
+import anylogger from 'anylogger';
+import parsefiles from '@mmstudio/an000041';
+import up from '@mmstudio/an000043';
+
+const logger = anylogger('@mmstudio/an000045');
+
+export default async function upload(req: IncomingMessage) {
+	logger.debug('start uploading files');
+	const files = await parsefiles(req);
+	logger.debug('files:', files);
+	const uploaded = await up(files.map((it) => {
+		return {
+			...it,
+			meta: {}
+		};
+	}));
+	logger.info('upload all!');
+	return uploaded;
 }
