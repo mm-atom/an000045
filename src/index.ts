caaab7edf9aa1dfac800ab14b1e4318693accb1c
taoqf <tao_qiufeng@126.com>
2022-09-24 17:50:43 +0800

    secret file

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import up from '@mmstudio/an000043';
 
 const logger = anylogger('@mmstudio/an000045');
 
-export default async function upload(req: IncomingMessage) {
+export default async function upload(req: IncomingMessage, secrete: boolean) {
 	logger.debug('start uploading files');
 	const files = await parsefiles(req);
 	logger.debug('files:', files);
@@ -14,7 +14,7 @@ export default async function upload(req: IncomingMessage) {
 			...it,
 			meta: {}
 		};
-	}));
+	}), secrete);
 	logger.info('upload all!');
 	return uploaded;
 }
